7041091cbed0c6373ce5d37d9a23590c5cad9cfe
Ronron-coder <royronan83@gmail.com>
2025-02-10 20:58:47 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function Number() {
+  var myVar = 40; 
+  if (true) {
+    let myLet = 30;   
+    const myConst = 50; 
+    console.log(myVar); 
+    console.log(myLet); 
+    console.log(myConst); 
+  }
+  console.log(myLet); 
+}
+Number();
